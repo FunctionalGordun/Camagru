046e0182f3f576125a404030f1b65fc10433e0ea
Gordey <gordey.moskvichev@gmail.com>
2020-11-16 22:11:44 +0300

    html css разметка

---
+++ add.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./assets/css/add.css">
+	<title>Add</title>
+</head>
+<body>
+	<?php include "header.php" ?>
+	<div class="add_container">
+		<div class="block1">
+			<div class="button_container">
+				<button>Upload file</button>
+				<button>Make photo</button>
+			</div>
+			<div class="photo_viewer">
+				<img src="./assets/img/2IEqr_kwt68.jpg">
+			</div>
+			<button>PUBLISH</button>
+		</div>
+		<div class="block2">
+			
+		</div>
+	</div>
+</body>
+</html>
